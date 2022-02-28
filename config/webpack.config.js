5578e2cc5e316ebdc6783789f176952c06dbc17e
Wesley Bliss <wesley.bliss@gmail.com>
2022-02-28 12:49:02 -0500

    WIP

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,3 @@
-const fs = require('fs')
-const path = require('path')
 const pkg = require('../package.json')
 const paths = require('./paths')
 const rules = require('./rules')
--- index.js
+++
@@ -1,3 +0,0 @@
-/* export { default as StorageProvider } from './providers/StorageProvider'
-export { default as LocalStorageProvider } from './providers/LocalStorageProvider' */
-export * from './react-wire-persisted'
