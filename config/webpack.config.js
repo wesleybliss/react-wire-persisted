5def336fed5b87b961793ff6fad318e6a8f2064a
Wesley Bliss <wesley.bliss@gmail.com>
2022-02-27 22:07:26 -0500

    Build update

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -11,51 +11,9 @@ const outputFile = (process.env.NODE_ENV === 'production')
     ? `${pkg.name}.min.js`
     : `${pkg.name}.js`
 
-const componenentExcludes = [
-    'src/index.js',
-    'stories.jsx',
-]
-
-/**
- * Builds a list of components so Webpack can create
- * separate entries & transpiled files for individual import
- * 
- * @param {String} root Root directory to start with
- * @returns {Array} List of entries
- */
-const buildComponentList = root => {
-    
-    let list = []
-    const nodes = fs.readdirSync(root)
-    
-    nodes.forEach(it => {
-        const node = path.resolve(root, it)
-        if (componenentExcludes.some(ex => it.endsWith(ex))) return
-        if (fs.statSync(node).isFile())
-            list.push(node)
-        else if (fs.statSync(node).isDirectory())
-            list = [ ...list, ...buildComponentList(node) ]
-    })
-    
-    return list
-    
-}
-
-const makeEntry = file => {
-    const name = file.replace(`${paths.src()}/`, '')
-    return name.substring(0, name.length - path.extname(name).length) // + `.min`
-}
-
-const components = buildComponentList(paths.src())
-const entries = components.reduce((acc, it) => ({
-    ...acc,
-    [makeEntry(it)]: it,
-}), {})
-
 const config = {
     mode: process.env.NODE_ENV,
     entry: [paths.src('index.js')],
-    // entry: entries,
     target: 'web',
     output: {
         clean: true,
