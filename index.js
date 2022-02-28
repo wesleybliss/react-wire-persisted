eb5d3d2a48b22dc25fb0d3944d328be8bc2dc9d4
Wesley Bliss <wesley.bliss@gmail.com>
2022-02-27 21:00:06 -0500

    Build update

---
+++ index.js
@@ -0,0 +1,3 @@
+/* export { default as StorageProvider } from './providers/StorageProvider'
+export { default as LocalStorageProvider } from './providers/LocalStorageProvider' */
+export * from './react-wire-persisted'
